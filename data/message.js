bb276882fb634c675a32c45581f7544b9e5c646a
Akinnawo Opeyemi <ope.akinnawo@gmail.com>
2019-12-18 05:53:36 +0100

    config update and sample message data added

---
+++ data/message.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    id: 'test_1',
+    content: 'content 1',
+  },
+  {
+    id: 'test_2',
+    content: 'content 2',
+  },
+  {
+    id: 'test_3',
+    content: 'content 3',
+  },
+  {
+    id: 'test_4',
+    content: 'content 4',
+  },
+  {
+    id: 'test_5',
+    content: 'content 5',
+  },
+];
