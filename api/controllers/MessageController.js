44a671a88ee3df09e6bf730eed1f74b73c137172
Akinnawo Opeyemi <ope.akinnawo@gmail.com>
2019-12-18 05:51:55 +0100

    message api created

---
+++ api/controllers/MessageController.js
@@ -0,0 +1,18 @@
+/**
+ * MessageController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  get: async function(req, res) {},
+
+  getAll: async function(req, res) {},
+
+  add: async function(req, res) {},
+
+  update: async function(req, res) {},
+
+  del: async function(req, res) {},
+};
---
+++ api/models/Message.js
@@ -0,0 +1,25 @@
+/**
+ * Message.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    id: { type: 'string', required: true },
+    content: { type: 'string' },
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+  },
+};
+
